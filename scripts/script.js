98c2d86f0f2ee5da058eb525409567b62a34a959
John Lestock <lestock17@gmail.com>
2019-08-30 18:23:10 -0500

    Setup HTML, employeeArray Push

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,77 @@
 console.log('JS File');
 
-window.onload = function() {
-    if (window.jQuery) {  
-        // jQuery is loaded  
-        console.log('jQuery is ready'); 
-    } else {
-        // jQuery is not loaded
-        console.log('jQuery is no ready');   
+const employeeArray = [
+    {
+        firstName: 'Harry',
+        lastName: 'Nilsson',
+        employeeID: '1941',
+        employeeTitle: 'Song Writer',
+        annualSalary: '$25'
     }
-}
+]; // end employeeArray
+
+$( document ).ready( onReady );
+
+function onReady() {
+    console.log('Here is the onReady');
+    $( '#submitBtn' ).on( 'click', handleSubmit );
+    listOutEmployees();
+} // end onReady
+
+
+function handleSubmit() {
+    console.log('Submitted');
+    // Submission 
+    let fNameValue = $( '#fName' ).val();
+    let lNameValue = $( '#lName' ).val();
+    let iDValue = $( '#iD' ).val();
+    let titleValue = $( '#title' ).val();
+    let aSalaryValue = $( '#aSalary' ).val();
+
+    // new Object
+    let newObject = {
+        firstName: fNameValue,
+        lastName: lNameValue,
+        employeeID: iDValue,
+        employeeTitle: titleValue,
+        annualSalary: aSalaryValue
+    }; // end newEmployee
+
+    // push
+
+    employeeArray.push(newObject);
+    console.log(employeeArray);
+    listOutEmployees();
+
+    // clear values
+    $( 'fName' ).val('');
+    $( 'lName' ).val('');
+    $( 'iD' ).val('');
+    $( 'title' ).val('');
+    $( 'aSalary' ).val('');
+
+} // end handleSubmit
+
+function listOutEmployees() {
+    $( '#employeeData' ).empty();
+
+    for ( person of employeeArray ) {
+        console.log(person.firstName);
+        let $rowItem = $(
+        `<td>${person.firstName}</td>
+        <td>${person.lastName}</td>
+        <td>${person.employeeID}</td>
+        <td>${person.employeeTitle}</td>
+        <td>${person.annualSalary}</td>
+        <td>Delete Button</td>`
+        );
+        
+        $rowItem.data(person.firstName, person.lastName, person.employeeID, person.employeeTitle, person.annualSalary);
+
+        $( '#employeeData' ).append( $rowItem );
+
+    }
+
+    
+} // end listEmployees
 
